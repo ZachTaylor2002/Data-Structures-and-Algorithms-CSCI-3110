1e81a8be86a3e93520c3598927965654a844c5db
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2023-11-16 13:44:53 -0600

    type.cpp

---
+++ Project 4/type.cpp
@@ -0,0 +1,28 @@
+// Implement the struct type defined in type.h here
+#include "type.h"
+#include <fstream>
+#include <iostream> //Preprocessor directives
+using namespace std;
+// Overloaded < operator that returns a boolean result if the the destination on
+// the left handed side it less that the destination on the rhs side
+bool flightRec::operator<(const flightRec &rhs) const {
+  if (destination < rhs.destination) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Overloaded operator that returns a boolean result if the two destinations and
+// origins are equal.
+bool flightRec::operator==(const flightRec &rhs) const {
+  if ((origin == rhs.origin) && (destination == rhs.destination)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Overloading the << operator to print out the flight information
+ostream &operator<<(ostream &os, const flightRec &f) {
+  os << f.origin << f.flightNum << f.destination << f.price;
+  return os;
+}
