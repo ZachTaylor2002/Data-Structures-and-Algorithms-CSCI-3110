724f629e70d35367367c3a5bb259db3313106986
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2023-11-16 12:58:46 -0600

    Create type.h

---
+++ Project 1/type.h
@@ -0,0 +1,13 @@
+// Declare the student type in this header file
+// Include this file in your main program file
+struct StudentType
+{
+	std::string ID;
+	std::string Name;
+	int CLA;
+	int OLA;
+	int Quiz;
+	int Homework;
+	int Exam;
+	int Bonus;
+};
